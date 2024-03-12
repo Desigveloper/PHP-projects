b34ccbc4cb65ed98ba7a79308062dd114317748a
Habibu Mamudu <habibumamudu@gmail.com>
2024-03-12 03:45:20 +0000

    updated days_calc

--- lab_exercise/days_calc.php
+++ lab_exercise/days_calc.php
@@ -1,25 +1,27 @@
 <?php
-$curr_month = 3;
-$curr_year = 2024;
-$days_of_curr_month = 12;
-$remaining_days_of_birth_month = 9;
-$birth_month = 12;
-$birth_year = 1991;
-$total_days = $days_of_curr_month + $remaining_days_of_birth_month;
+  # a program to calculate the number of days between date of birth and current date
+  
+  $curr_month = 3;
+  $curr_year = 2024;
+  $days_of_curr_month = 12;
+  $remaining_days_of_birth_month = 9;
+  $birth_month = 12;
+  $birth_year = 1991;
+  $total_days = $days_of_curr_month + $remaining_days_of_birth_month;
 
-for (; $birth_year <= $curr_year; $birth_month++){
-  if ($birth_month > 12){
-    $birth_month = 1;
-    ++$birth_year;
-    $total_days += cal_days_in_month(CAL_GREGORIAN, $birth_month, $birth_year);
-  }
-  else {
-    $total_days += cal_days_in_month(CAL_GREGORIAN, $birth_month, $birth_year);
-  }
+  for (; $birth_year <= $curr_year; $birth_month++){
+    if ($birth_month > 12){
+      $birth_month = 1;
+      ++$birth_year;
+      $total_days += cal_days_in_month(CAL_GREGORIAN, $birth_month, $birth_year);
+    }
+    else {
+      $total_days += cal_days_in_month(CAL_GREGORIAN, $birth_month, $birth_year);
+    }
 
-  if ($birth_year == $curr_year && $birth_month == $curr_month){
-    break;
-  }
+    if ($birth_year == $curr_year && $birth_month == $curr_month){
+      break;
+    }
 }
 
 echo $total_days . "\n";
